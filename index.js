8930c5233277089919490aa5639e0cccb0928847
Zeeshan-2k1 <ashraf2k1zeeshan@gmail.com>
2023-03-27 18:29:42 +0530

    fix: db update

--- index.js
+++ index.js
@@ -1,31 +1,16 @@
 const jsonServer = require('json-server');
 const cors = require('cors');
-const express = require('express');
-const fs = require('fs');
-const fileupload = require('express-fileupload');
 
 const server = jsonServer.create();
-const router = jsonServer.router('/tmp/db.json');
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(cors());
 server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use(router);
-server.use(
-  fileupload({
-    useTempFiles: true,
-    tempFileDir: '/tmp',
-  })
-);
 
 const PORT = 3004;
-
-fs.writeFile(
-  '/tmp/db.json',
-  JSON.stringify({ buckets: [], history: [] }),
-  (err) => {}
-);
 
 server.listen(PORT, () => {
   console.log(`JSON Server is running on http://localhost:${PORT}`);
