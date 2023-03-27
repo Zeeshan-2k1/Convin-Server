161bf75386a2c597e629f2a0b5bbb2ed9bea9cd6
Zeeshan-2k1 <ashraf2k1zeeshan@gmail.com>
2023-03-27 15:40:39 +0530

    fix: mock db

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const jsonServer = require('json-server');
 const cors = require('cors');
 const express = require('express');
+const fs = require('fs');
 const fileupload = require('express-fileupload');
 
 const server = jsonServer.create();
@@ -19,6 +20,12 @@ server.use(
 );
 
 const PORT = 3004;
+
+fs.writeFile(
+  '/tmp/db.json',
+  JSON.stringify({ buckets: [], history: [] }),
+  (err) => {}
+);
 
 server.listen(PORT, () => {
   console.log(`JSON Server is running on http://localhost:${PORT}`);
